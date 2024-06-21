7a76d1373034805e00294a4ea567bdd967fdc786
Nathali Perez <nathalipf2803@ggmail.com>
2024-06-21 11:00:35 -0400

    Break the sections into React components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      <nav />
-      <main />
-      <footer />
+      <Nav />
+      <Main />
+      <Footer />
     </>
   );
 }
