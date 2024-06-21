aa42cecd06cdd0215cb59dacc0c93a1e8e7bc2a9
Nathali Perez <nathalipf2803@ggmail.com>
2024-06-21 11:13:49 -0400

    Add logo and navigation elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
         <header>
-            <h1>This is the header</h1>
+            <img src={logo} alt="Little Lemon Logo" />
+            <h1>Little Lemon</h1>
         </header>
     );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,9 @@ const Nav = () => {
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+                <li><a href="/order-online">Order Online</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
         </nav>
