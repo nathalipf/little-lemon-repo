10e21bbe303c85ed4a41de4a772a8bf8be70c653
Nathali Perez <nathalipf2803@ggmail.com>
2024-06-20 23:53:00 -0400

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <Nav />
-      <Main />
-      <Footer />
+      <nav />
+      <main />
+      <footer />
     </>
   );
 }
