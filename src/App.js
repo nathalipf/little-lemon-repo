66701bfb9a955337d77c0f1ae3e5b04708a79bf2
Nathali Perez <nathalipf2803@ggmail.com>
2024-06-20 23:02:50 -0400

    Set up the initial semantic structure

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Nathali's Home Page
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
