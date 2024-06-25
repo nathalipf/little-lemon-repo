53c4b0adcaf332b62b2371ff8054fcecb95d07d8
Nathali Perez <nathalipf2803@ggmail.com>
2024-06-24 23:06:53 -0400

    App.css updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import logo from '../assets/logo.svg'
 
 const Nav = () => {
     return (
         <nav>
             <a href='/'>
-                <img src='' />
+                <img src={logo} alt='logo' />
             </a>
+
+            {/*mobile navbar */}
+            <div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+
+            {/*nav items */}
+            <ul>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/'>About</a>
+                </li>
+                <li>
+                    <a href='/'>Services</a>
+                </li>
+                <li>
+                    <a href='/'>Menu</a>
+                </li>
+                <li>
+                    <a href='/'>Reservations</a>
+                </li>
+                <li>
+                    <a href='/'>Order Online</a>
+                </li>
+                <li>
+                    <a href='/'>Login</a>
+                </li>
+            </ul>
 
         </nav>
     )
