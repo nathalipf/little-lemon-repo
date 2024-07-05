7aecde101ee737314e707f94f832a7028bd2a525
Nathali Perez <nathalipf2803@ggmail.com>
2024-07-05 17:42:07 -0400

    Updates in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.svg'
 
 const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
-        <nav>
-            <a href='/'>
+        <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+            <a href='/' className="logo">
                 <img src={logo} alt='logo' />
             </a>
 
             {/*mobile navbar */}
-            <div>
+            <div className="menu-icon" onClick={toggleMenu}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
             </div>
 
             {/*nav items */}
-            <ul>
+            <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
                 <li>
                     <a href='/'>Home</a>
                 </li>
