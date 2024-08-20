981ee6a8d6a6403e10767e53917d39a92b11827b
Nathali Perez <nathalipf2803@ggmail.com>
2024-08-20 18:28:33 -0400

    Footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import logo from '../assets/logo.svg';
 
 const Footer = () => {
     return (
         <footer>
-            <div className="footer-content">
-                <p>Contact us:</p>
-                <ul>
-                    <li>Email: info@littlelemon.com</li>
-                    <li>Phone: +1 234 567 890</li>
-                </ul>
-            </div>
-            <div className="footer-links">
-                <ul>
-                    <li><a href="/terms">Terms of Service</a></li>
-                    <li><a href="/privacy">Privacy Policy</a></li>
-                    <li><a href="/sitemap">Sitemap</a></li>
-                </ul>
-            </div>
-            <div className="footer-bottom">
-                <p>&copy; 2024 Little Lemon. All rights reserved.</p>
-            </div>
+            <section>
+                <div className='company-info'>
+                    <img src={logo} alt='' />
+                    <p>We are a family owned Mediterranean restaurant, focused on traditioal recipes served with a modern twist.</p>
+                </div>
+                <div>
+                    <h3>Important Links</h3>
+                    <ul>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/'>About</a></li>
+                        <li><a href='/'>Menu</a></li>
+                        <li><a href='/'>Reservations</a></li>
+                        <li><a href='/'>Order Online</a></li>
+                        <li><a href='/'>Login</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>Email: <br /> info@littlelemon.com</li>
+                        <li>Phone: <br /> +1 234 567 890</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Social Media</h3>
+                    <ul>
+                        <li><a href='/'>Facebook</a></li>
+                        <li><a href='/'>Instagram</a></li>
+                    </ul>
+                </div>
+            </section>
         </footer>
     );
 };
